cd8edc9b08c7d4b6c40ba0326ba5aaa1d1c7d3a4
fred wright <fredwright0@gmail.com>
2020-11-23 13:53:23 +0000

    Improve tests

--- packages/sdk/src/_tests/e2e.tests.ts
+++ packages/sdk/src/_tests/e2e.tests.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import dotenv from "dotenv";
 import { createLinearSdk, LinearStatus } from "../index";
+import { LinearResponse } from "../_generated/sdk-api";
 
 dotenv.config();
 
@@ -19,7 +20,7 @@ function getSdk() {
 async function getTeamSdk() {
   const sdk = getSdk();
   const teams = await sdk.teams();
-  expect(teams.status).toBe(LinearStatus.success);
+  expect(teams.status).toEqual(LinearStatus.success);
 
   const first = teams.data?.nodes[0];
   expect(first).toBeDefined();
@@ -35,7 +36,7 @@ async function getTeamSdk() {
 async function getIssueSdk() {
   const sdk = getSdk();
   const issues = await sdk.issues();
-  expect(issues.status).toBe(LinearStatus.success);
+  expect(issues.status).toEqual(LinearStatus.success);
 
   const first = issues.data?.nodes[0];
   expect(first).toBeDefined();
@@ -45,40 +46,72 @@ async function getIssueSdk() {
   return issue;
 }
 
+/**
+ * Assert success of the sdk operation
+ */
+function expectSuccess<T, V>(response: LinearResponse<T, V>) {
+  expect(response.status).toEqual(LinearStatus.success);
+  expect(response.data).toBeDefined();
+  expect(response.error).toBeUndefined();
+  expect(response.statusCode).toEqual(200);
+  expect(response.errors).toBeUndefined();
+  expect(response.extensions).toBeUndefined();
+  expect(response.query).toBeUndefined();
+  expect(response.variables).toBeUndefined();
+}
+
+/**
+ * Assert failure of the sdk operation
+ */
+function expectError<T, V>(
+  response: LinearResponse<T, V>,
+  { errorMessage, queryName, variables }: { errorMessage: string; queryName: string; variables: V }
+) {
+  expect(response.status).toEqual(LinearStatus.error);
+  expect(response.data).toBeUndefined();
+  expect(response.statusCode).toEqual(200);
+  expect(response.errors?.[0].message).toEqual(expect.stringContaining(errorMessage));
+  expect(response.query).toEqual(expect.stringContaining(`query ${queryName}`));
+  expect(response.variables).toEqual(expect.objectContaining(variables));
+}
+
 if (process.env.E2E_API_KEY) {
   describe("end-to-end", () => {
+    it("query for the viewer", async () => {
+      const sdk = getSdk();
+      const viewer = await sdk.viewer();
+      expectSuccess(viewer);
+    });
+
     it("query for a team", async () => {
       const team = await getTeamSdk();
-      expect(team.status).toBe(LinearStatus.success);
-      expect(team.data?.id).toBeDefined();
-      expect(team.error).toBeUndefined();
+      expectSuccess(team);
     });
 
     it("query for fake team", async () => {
       const sdk = getSdk();
-      const team = await sdk.team("not a real id");
-
-      expect(team.status).toBe(LinearStatus.error);
-      expect(team.data).toBeUndefined();
-      expect(team.statusCode).toBe(200);
-      expect(team.errors?.[0].message).toBe("Entity not found");
+      const team = await sdk.team("not a real team id");
+      expectError(team, {
+        errorMessage: "Entity not found",
+        queryName: "team",
+        variables: { id: "not a real team id" },
+      });
     });
 
     it("query for an issue", async () => {
       const issue = await getIssueSdk();
-      expect(issue.status).toBe(LinearStatus.success);
-      expect(issue.data?.id).toBeDefined();
-      expect(issue.error).toBeUndefined();
+      expectSuccess(issue);
     });
 
     it("query for fake issue", async () => {
       const sdk = getSdk();
-      const issue = await sdk.issue("not a real id");
+      const issue = await sdk.issue("not a real issue id");
 
-      expect(issue.status).toBe(LinearStatus.error);
-      expect(issue.data).toBeUndefined();
-      expect(issue.statusCode).toBe(200);
-      expect(issue.errors?.[0].message).toBe("Entity not found");
+      expectError(issue, {
+        errorMessage: "Entity not found",
+        queryName: "issue",
+        variables: { id: "not a real issue id" },
+      });
     });
   });
 }
--- packages/sdk/src/_tests/utils.tests.ts
+++ packages/sdk/src/_tests/utils.tests.ts
@@ -3,12 +3,12 @@ import { serializeUserAgent } from "../utils";
 describe("serializeUserAgent", () => {
   it("handles empty", async () => {
     const result = serializeUserAgent({});
-    expect(result).toBe("");
+    expect(result).toEqual("");
   });
 
   it("handles one property", async () => {
     const result = serializeUserAgent({ "@linear/sdk": "0.1.2" });
-    expect(result).toBe("@linear/sdk@0.1.2");
+    expect(result).toEqual("@linear/sdk@0.1.2");
   });
 
   it("handles multiple properties", async () => {
@@ -16,13 +16,13 @@ describe("serializeUserAgent", () => {
       "@linear/sdk": "0.1.2",
       "@linear/dfl": "1.2.3",
     });
-    expect(result).toBe("@linear/sdk@0.1.2 @linear/dfl@1.2.3");
+    expect(result).toEqual("@linear/sdk@0.1.2 @linear/dfl@1.2.3");
   });
 
   it("handles uri encoding of value", async () => {
     const result = serializeUserAgent({
       "test/@1];a": '@/22]";qwed',
     });
-    expect(result).toBe("test/@1];a@%40%2F22%5D%22%3Bqwed");
+    expect(result).toEqual("test/@1];a@%40%2F22%5D%22%3Bqwed");
   });
 });
