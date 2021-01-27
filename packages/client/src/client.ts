d7b0cc987800e53a0ca8f12937dda9a6886291d5
fred wright <fredwright0@gmail.com>
2021-01-27 19:28:19 +0000

    Add LinearError wrapper

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,5 +1,6 @@
 import { DocumentNode, print } from "graphql";
 import { GraphQLClient } from "graphql-request";
+import { LinearError } from "./error";
 import { LinearClientOptions, LinearClientParsedOptions } from "./types";
 import { serializeUserAgent } from "./utils";
 import { LinearSdk } from "./_generated/_gen_sdk";
@@ -12,13 +13,18 @@ import { LinearSdk } from "./_generated/_gen_sdk";
  */
 function parseClientOptions({ apiKey, accessToken, baseUrl, ...opts }: LinearClientOptions): LinearClientParsedOptions {
   if (!accessToken && !apiKey) {
-    throw new Error("No accessToken or apiKey provided");
+    throw new Error("No accessToken or apiKey provided to the LinearClient");
   }
 
   return {
     headers: {
       /** Use bearer if oauth token exists, otherwise use the provided apiKey */
-      Authorization: accessToken ? `Bearer ${accessToken}` : apiKey ?? "",
+      Authorization: accessToken
+        ? accessToken.startsWith("Bearer ")
+          ? accessToken
+          : `Bearer ${accessToken}`
+        : apiKey ?? "",
+      /** Use configured headers */
       ...opts.headers,
       /** Override any user agent with the sdk name and version */
       "User-Agent": serializeUserAgent({
@@ -34,20 +40,25 @@ function parseClientOptions({ apiKey, accessToken, baseUrl, ...opts }: LinearCli
 /**
  * Create a Linear API client
  *
- * @param options request options to pass to the graphql client
- * @returns SDK for interacting with the Linear API
+ * @param options request options to pass to the graphql-request client
  */
 export class LinearClient extends LinearSdk {
   public options: LinearClientParsedOptions;
   public client: GraphQLClient;
 
   public constructor(options: LinearClientOptions) {
-    const opts = parseClientOptions(options);
-    const client = new GraphQLClient(opts.baseUrl, opts);
+    const parsedOptions = parseClientOptions(options);
+    const graphQLClient = new GraphQLClient(parsedOptions.baseUrl, parsedOptions);
 
-    super(<R, V>(doc: DocumentNode, vars?: V) => this.client.request<R, V>(print(doc), vars));
+    super(<R, V>(doc: DocumentNode, vars?: V) =>
+      /** Call the graphql-request client */
+      this.client.request<R, V>(print(doc), vars).catch(error => {
+        /** Wrap any errors */
+        throw new LinearError(error);
+      })
+    );
 
-    this.options = opts;
-    this.client = client;
+    this.options = parsedOptions;
+    this.client = graphQLClient;
   }
 }
--- packages/client/src/error.ts
+++ packages/client/src/error.ts
@@ -1,13 +1,97 @@
-import { LinearErrorType } from "./types";
+import { getKeyByValue } from "@linear/plugin-common";
+import { LinearErrorRaw, LinearErrorType, LinearGraphQLErrorRaw } from "./types";
+
+/**
+ * A map between the Linear API string type and the LinearErrorType enum
+ */
+const errorMap: Record<LinearErrorType, string> = {
+  [LinearErrorType.FeatureNotAccessible]: "feature not accessible",
+  [LinearErrorType.InvalidInput]: "invalid input",
+  [LinearErrorType.Ratelimited]: "ratelimited",
+  [LinearErrorType.NetworkError]: "network error",
+  [LinearErrorType.AuthenticationError]: "authentication error",
+  [LinearErrorType.Forbidden]: "forbidden",
+  [LinearErrorType.BootstrapError]: "bootstrap error",
+  [LinearErrorType.Unknown]: "unknown",
+  [LinearErrorType.InternalError]: "internal error",
+  [LinearErrorType.Other]: "other",
+  [LinearErrorType.UserError]: "user error",
+  [LinearErrorType.GraphqlError]: "graphql error",
+  [LinearErrorType.LockTimeout]: "lock timeout",
+};
+
+/**
+ * Match the error type or return unknown
+ */
+function getErrorType(type?: string): LinearErrorType {
+  return getKeyByValue(errorMap, type) ?? LinearErrorType.Unknown;
+}
+
+/**
+ * The error shown if no other message is available
+ */
+const defaultError = "Unknown error from LinearClient";
+
+/**
+ * One of potentially many graphql errors returned by the Linear API
+ *
+ * @error the raw graphql error returned on the error response
+ */
+export class LinearGraphQLError {
+  /** The type of this graphql error */
+  public type: LinearErrorType;
+  /** A friendly error message */
+  public message: string;
+  /** If this error is caused by the user input */
+  public userError?: boolean;
+  /** The path to the graphql node at which the error occured */
+  public path?: string[];
+
+  public constructor(error?: LinearGraphQLErrorRaw) {
+    this.type = getErrorType(error?.extensions?.type);
+    this.userError = error?.extensions?.userError;
+    this.path = error?.path;
+
+    /** Set best available message */
+    this.message =
+      error?.extensions?.userPresentableMessage ?? error?.message ?? error?.extensions?.type ?? defaultError;
+  }
+}
 
 /**
  * An error from the Linear API
+ *
+ * @param error a raw error returned from the graphql-request client
  */
 export class LinearError extends Error {
-  public type?: LinearErrorType;
+  /** The type of the first error returned by the Linear API */
+  public type: LinearErrorType;
+  /** A list of graphql errors returned by the Linear API */
+  public errors: LinearGraphQLError[];
+  /** The graphql query that caused this error */
+  public query?: string;
+  /** The graphql variables that caused this error */
+  public variables?: Record<string, unknown>;
+  /** Any data returned by this request */
+  public data?: unknown;
+  /** The http status of this request */
+  public status?: number;
 
-  public constructor(type: LinearErrorType, message: string) {
-    super(message);
-    this.type = type;
+  public constructor(error?: LinearErrorRaw) {
+    /** Parse graphql errors */
+    const errors = (error?.response?.errors ?? []).map(graphqlError => {
+      return new LinearGraphQLError(graphqlError);
+    });
+
+    /** Set best available message */
+    super(errors[0]?.message ?? error?.message ?? error?.name ?? defaultError);
+
+    /** Set error properties */
+    this.type = errors[0]?.type ?? LinearErrorType.Unknown;
+    this.status = error?.response?.status;
+    this.errors = errors;
+    this.query = error?.request?.query;
+    this.variables = error?.request?.variables;
+    this.data = error?.response?.data;
   }
 }
