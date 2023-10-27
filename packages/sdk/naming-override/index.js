d18a4d18394b30bc4f488dba6b5f823ea82b8c1b
Guillaume Lachaud <guillaume@linear.app>
2023-10-27 09:00:00 -0700

    Workaround to handle schema with duplicate queries (#401)
    * Attempt to override naming convention for ProjectMilestoneQuery
    
    * Attempt to override naming convention for ProjectMilestoneQuery
    
    * Deduplicate query args type names
    
    We want to replace the `ProjectMilestone(s)?` query with
    `projectMilestone(s)?`. To do this, we'll have a period where both
    queries are in our API schema, which generates duplicate type names and
    fails to build.
    
    As a hack, to work around this, we can simply append a counter to those
    known-duplicate type names when we encounter them for the (N > 0)-th
    time, and so let the SDK build. The types are identical otherwise so
    there's no functional difference, just a bit of noise in the generated
    output.
    
    * docs(changeset): override naming convention to allow support of duplicate queries
    
    ---------
    
    Co-authored-by: Sean McGivern <sean@linear.app>

---
+++ packages/sdk/naming-override/index.js
@@ -0,0 +1,19 @@
+const changeCaseAll = require("change-case-all");
+
+// Sometimes we have two queries with the same case. If we do, add the query names here (PascalCased), and we'll perform
+// deduplication on their query args to allow the SDK to build by suffixing additional types with the same name with a
+// counter.
+const duplicateQueries = ["ProjectMilestone", "ProjectMilestones"];
+
+// Object containing counters of how many times we've seen each DuplicateQueryNameArgs type.
+const deduplicate = Object.fromEntries(duplicateQueries.map(query => [`Query${query}Args`, 0]));
+
+exports.case = (type) => {
+  if (type in deduplicate) {
+    if (deduplicate[type] > 0) {
+      type = type + deduplicate[type];
+    }
+    deduplicate[type] += 1
+  }
+  return changeCaseAll.pascalCase(type)
+}
