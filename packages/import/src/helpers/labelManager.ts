bd95aaf5846d9cf074ff820ee1e6f2786c44d4c9
Mufeez Amjad <mufeez.amjad@outlook.com>
2024-11-13 20:27:05 +0300

    cleanup

--- packages/import/src/helpers/labelManager.ts
+++ packages/import/src/helpers/labelManager.ts
@@ -5,113 +5,26 @@ import _ from "lodash";
 type Id = string;
 const WORKSPACE_ID = "workspace";
 
-class LabelManager {
-  private nameToLabel: Record<string, { [teamId: Id | typeof WORKSPACE_ID]: Label }> = {};
-  private idToLabel: Record<Id, { [teamId: Id | typeof WORKSPACE_ID]: Label }> = {};
+type LabelType = "root" | "parent" | "child";
 
-  public constructor(
-    existingLabels: IssueLabel[],
-    private teamId: Id
-  ) {
-    this.initializeLabels(existingLabels);
-  }
-
-  public getLabelByName(name: string, teamId: Id | typeof WORKSPACE_ID): Label | undefined {
-    return this.nameToLabel[name.toLowerCase()]?.[teamId] ?? this.nameToLabel[name]?.[WORKSPACE_ID];
-  }
-
-  public getLabelById(id: Id, teamId: Id | typeof WORKSPACE_ID): Label | undefined {
-    return this.idToLabel[id]?.[teamId] ?? this.idToLabel[id]?.[WORKSPACE_ID];
-  }
-
-  private getLabel<T>(props: { name: string } | { id: Id }, checkInstance: (label: Label) => boolean): T | undefined {
-    let label: Label | undefined;
-    if ("name" in props) {
-      label = this.getLabelByName(props.name, this.teamId);
-    } else {
-      label = this.getLabelById(props.id, this.teamId);
-    }
-    return (label && checkInstance(label) ? label : undefined) as T | undefined;
-  }
-
-  public getGroupLabel(props: { name: string } | { id: Id }): LabelGroup | undefined {
-    return this.getLabel<LabelGroup>(props, label => label instanceof LabelGroup);
-  }
-
-  public getRootLabel(props: { name: string } | { id: Id }): Label | undefined {
-    return this.getLabel<Label>(props, label => label instanceof Label);
-  }
-
-  public addLabel(props: { label: Label; parent?: LabelGroup; teamId?: Id | typeof WORKSPACE_ID }) {
-    const { label, parent, teamId = this.teamId } = props;
-
-    this.nameToLabel[label.name.toLowerCase()] = { [teamId]: label };
-    this.idToLabel[label.id] = { [teamId]: label };
-
-    if (parent) {
-      parent.addLabel(label.name, label.id);
-    }
-  }
-
-  private async initializeLabels(existingLabels: IssueLabel[]) {
-    // We want to process groups first.
-    existingLabels.sort(a => (a.isGroup ? -1 : 1));
-
-    for (const existingLabel of existingLabels) {
-      const labelName = existingLabel.name?.toLowerCase();
-      const teamId = (await existingLabel.team)?.id ?? WORKSPACE_ID;
-
-      if (existingLabel.isGroup && labelName && existingLabel.id) {
-        const group = new LabelGroup(existingLabel.id, labelName);
-        this.addLabel({ label: group, teamId });
-      } else if (labelName && existingLabel.id) {
-        const parent = await existingLabel.parent;
-
-        if (parent) {
-          const group = this.idToLabel[parent.id]?.[teamId] as LabelGroup;
-          this.addLabel({ label: new Label(existingLabel.id, labelName), parent: group, teamId });
-        } else {
-          this.addLabel({ label: new Label(existingLabel.id, labelName), teamId });
-        }
-      }
-    }
-  }
-}
-
-const createLabel = async (
-  client: LinearClient,
-  {
-    name,
-    description,
-    color,
-    parentId,
-    teamId,
-  }: {
-    name: string;
-    description?: string;
-    color?: string;
-    parentId?: Id;
-    teamId: Id;
-  }
-) => {
-  const response = await client.createIssueLabel({ name, description, color, teamId, parentId });
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return (await response?.issueLabel)!.id;
-};
-
-const deleteLabel = async (client: LinearClient, labelId: Id) => {
-  await client.deleteIssueLabel(labelId);
-};
-
+/**
+ * Handle importing labels
+ *
+ * @param client Linear client instance to use for API requests
+ * @param importData Import data containing issues and labels
+ * @param teamId Team ID being imported to
+ * @param existingLabels Existing labels in the team and workspace
+ */
 export const handleLabels = async (
   client: LinearClient,
   importData: ImportResult,
   teamId: string,
   existingLabels: IssueLabel[]
-): Promise<Record<string, { type: "root" | "parent" | "child"; id: Id }>> => {
+): Promise<Record<string, { type: LabelType; id: Id }>> => {
   const manager = new LabelManager(existingLabels, teamId);
-  const labelMapping: Record<string, { type: "root" | "parent" | "child"; id: Id }> = {};
+  const labelMapping: Record<string, { type: LabelType; id: Id }> = {};
 
+  // We process issues instead of labels to validate issue <> label constraints (e.g. only one label from a group)
   for (const issue of importData.issues) {
     const labels = issue.labels;
     if (!labels) {
@@ -142,9 +55,8 @@ const handleIssueLabels = async (
     const group = parsed[0];
     let labelName = parsed[1];
 
-    // If this label's group is already used in this issue,
-    // or if it was previously mapped as a child label and its group is in use,
-    // create/use a root label with the full name
+    // If this label's group is already used in this issue, or if it was previously mapped as a child label
+    // and its group is in use, create/use a root label with the full name.
     if (group && (usedGroups.has(group) || (labelMapping[labelId]?.type === "child" && usedGroups.has(group)))) {
       const fullName = labelData.name;
       actualLabelId = manager.getLabelByName(fullName, teamId)?.id;
@@ -183,12 +95,12 @@ const handleIssueLabels = async (
           // Create the group label with a modified name
           const groupName = `${group} (imported)`;
           const groupId = await createLabel(client, { name: groupName, teamId });
-          groupLabel = new LabelGroup(groupId, groupName);
+          groupLabel = new GroupLabel(groupId, groupName);
           manager.addLabel({ label: groupLabel, teamId });
         } else {
           // Create new group label
           const groupId = await createLabel(client, { name: group, teamId });
-          groupLabel = new LabelGroup(groupId, group);
+          groupLabel = new GroupLabel(groupId, group);
           manager.addLabel({ label: groupLabel, teamId });
         }
       }
@@ -242,7 +154,8 @@ const handleIssueLabels = async (
 };
 
 function parseLabelName(fullName: string): [string | undefined, string] {
-  const parts = fullName.split("/");
+  // Ensure every part is truncated to 80 characters
+  const parts = fullName.split("/").map(part => _.truncate(part.trim(), { length: 80 }));
   let group: string | undefined;
   let subgroup: string;
 
@@ -254,13 +167,142 @@ function parseLabelName(fullName: string): [string | undefined, string] {
     subgroup = fullName;
   }
 
-  // Ensure every part is truncated to 80 characters
-  return [group, subgroup].map(part => (part ? _.truncate(part.trim(), { length: 80 }) : undefined)) as [
-    string | undefined,
-    string,
-  ];
+  return [group, subgroup] as [string | undefined, string];
 }
 
+class LabelManager {
+  private nameToLabel: Record<string, { [teamId: Id | typeof WORKSPACE_ID]: Label }> = {};
+  private idToLabel: Record<Id, { [teamId: Id | typeof WORKSPACE_ID]: Label }> = {};
+
+  public constructor(
+    existingLabels: IssueLabel[],
+    private teamId: Id
+  ) {
+    this.initializeLabels(existingLabels);
+  }
+
+  /**
+   * Retrieve a label by name in either the workspace or specified team
+   *
+   * @param name Label name
+   * @param teamId Team ID to search in
+   * @returns Label instance if found
+   */
+  public getLabelByName(name: string, teamId: Id | typeof WORKSPACE_ID): Label | undefined {
+    return this.nameToLabel[name.toLowerCase()]?.[teamId] ?? this.nameToLabel[name]?.[WORKSPACE_ID];
+  }
+
+  /**
+   * Retrieve a label by its ID in either the workspace or specified team
+   *
+   * @param name Label ID
+   * @param teamId Team ID to search in
+   * @returns Label instance if found
+   */
+  public getLabelById(id: Id, teamId: Id | typeof WORKSPACE_ID): Label | undefined {
+    return this.idToLabel[id]?.[teamId] ?? this.idToLabel[id]?.[WORKSPACE_ID];
+  }
+
+  /**
+   * Retrieve a group label by name or ID in either the workspace or specified team
+   *
+   * @param props Object containing either name or ID
+   * @returns GroupLabel instance if found
+   */
+  public getGroupLabel(props: { name: string } | { id: Id }): GroupLabel | undefined {
+    return this.getLabel(props, GroupLabel);
+  }
+
+  /**
+   * Retrieve a root label by name or ID in either the workspace or specified team
+   *
+   * @param props Object containing either name or ID
+   * @returns Label instance if found
+   */
+  public getRootLabel(props: { name: string } | { id: Id }): Label | undefined {
+    return this.getLabel(props, Label);
+  }
+
+  /**
+   * Adds a label to the manager
+   *
+   * @param props Object label, its parent (if any), and team ID (defaults to the current teamId)
+   */
+  public addLabel(props: { label: Label; parent?: GroupLabel; teamId?: Id | typeof WORKSPACE_ID }) {
+    const { label, parent, teamId = this.teamId } = props;
+
+    this.nameToLabel[label.name.toLowerCase()] = { [teamId]: label };
+    this.idToLabel[label.id] = { [teamId]: label };
+
+    if (parent) {
+      parent.addLabel(label.name, label.id);
+    }
+  }
+
+  private getLabel<T extends typeof Label>(
+    props: { name: string } | { id: Id },
+    instanceType: T
+  ): InstanceType<T> | undefined {
+    let label: Label | undefined;
+    if ("name" in props) {
+      label = this.getLabelByName(props.name, this.teamId);
+    } else {
+      label = this.getLabelById(props.id, this.teamId);
+    }
+    return (label && label instanceof instanceType ? label : undefined) as InstanceType<T> | undefined;
+  }
+
+  private async initializeLabels(existingLabels: IssueLabel[]) {
+    // We want to process groups first.
+    existingLabels.sort(a => (a.isGroup ? -1 : 1));
+
+    for (const existingLabel of existingLabels) {
+      const labelName = existingLabel.name?.toLowerCase();
+      const teamId = (await existingLabel.team)?.id ?? WORKSPACE_ID;
+
+      if (existingLabel.isGroup && labelName && existingLabel.id) {
+        const group = new GroupLabel(existingLabel.id, labelName);
+        this.addLabel({ label: group, teamId });
+      } else if (labelName && existingLabel.id) {
+        const parent = await existingLabel.parent;
+
+        if (parent) {
+          const group = this.idToLabel[parent.id]?.[teamId] as GroupLabel;
+          this.addLabel({ label: new Label(existingLabel.id, labelName), parent: group, teamId });
+        } else {
+          this.addLabel({ label: new Label(existingLabel.id, labelName), teamId });
+        }
+      }
+    }
+  }
+}
+
+const createLabel = async (
+  client: LinearClient,
+  {
+    name,
+    description,
+    color,
+    parentId,
+    teamId,
+  }: {
+    name: string;
+    description?: string;
+    color?: string;
+    parentId?: Id;
+    teamId: Id;
+  }
+) => {
+  const response = await client.createIssueLabel({ name, description, color, teamId, parentId });
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  return (await response?.issueLabel)!.id;
+};
+
+const deleteLabel = async (client: LinearClient, labelId: Id) => {
+  await client.deleteIssueLabel(labelId);
+};
+
+// A root label
 class Label {
   public constructor(
     public id: Id,
@@ -268,7 +310,8 @@ class Label {
   ) {}
 }
 
-class LabelGroup extends Label {
+// A label group (parent label)
+class GroupLabel extends Label {
   public labels: Record<string, Id> = {};
 
   public constructor(id: Id, name: string) {
@@ -280,4 +323,5 @@ class LabelGroup extends Label {
   }
 }
 
+// A label in a group (child label)
 class SubgroupLabel extends Label {}
