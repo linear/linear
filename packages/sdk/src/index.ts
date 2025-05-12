6eb381e324e6727f3009574f38001513aca6a648
Guillaume <guillaume@linear.app>
2025-05-12 14:15:41 -0700

    fix: do not package `LinearWebhooks` in UMD module

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,9 +1,2 @@
-/// <reference lib="dom"/>
-
-export * from "./client";
-export * from "./error";
-export * from "./graphql-client";
-export * from "./types";
-export * as LinearDocument from "./_generated_documents";
-export * from "./_generated_sdk";
+export * from "./index_umd";
 export * from "./webhooks";
