643309546cd1590f5e31ceaaf846107cc166e165
Steven DeMartini <1647130+sjdemartini@users.noreply.github.com>
2025-08-28 10:31:42 -0700

    chore: upgrade markdown-magic to latest (#860)
    Should fix some dependabot vulnerability warnings from transitive dependencies.

--- docs/markdown-magic.config.js
+++ docs/markdown-magic.config.js
@@ -2,6 +2,8 @@ const codeSectionTransform = require("./transforms/code-section");
 const textSectionTransform = require("./transforms/text-section");
 
 module.exports = {
+  open: "AUTO-GENERATED-CONTENT:START",
+  close: "AUTO-GENERATED-CONTENT:END",
   transforms: {
     CODE_SECTION: codeSectionTransform,
     TEXT_SECTION: textSectionTransform,
--- docs/transforms/text-section.js
+++ docs/transforms/text-section.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = function TEXT_SECTION(content, options, config) {
+module.exports = function TEXT_SECTION({ options, srcPath }) {
   let text;
   let syntax = options.syntax;
   if (!options.id || !options.src) {
     return false;
   }
 
-  const fileDir = path.dirname(config.originalPath);
-  const filePath = path.join(fileDir, options.src);
+  const fileDir = path.dirname(srcPath);
+  const filePath = path.resolve(fileDir, options.src);
 
   try {
     text = fs.readFileSync(filePath, "utf8", (err, contents) => {
